309a4aac20b5f1911d22c652e35151daa85e59c0
manu <manuellatourf@gmail.com>
2024-12-14 11:56:52 +0100

    sombra conseguida

--- script/adjustBoxSizes.js
+++ script/adjustBoxSizes.js
@@ -1,14 +1,21 @@
 function adjustBoxSizes() {
-    const boxes = document.querySelectorAll('.box');
-    const aspectRatio = 1.55; // Relación de aspecto ancho:alto (1.55:1)
+  const boxes = document.querySelectorAll('.box');
+  const preboxes = document.querySelectorAll('.pre-box');
+  const aspectRatio = 1.55; // Relación de aspecto ancho:alto (1.55:1)
 
-    boxes.forEach(box => {
-      const width = box.offsetWidth; // Obtiene el ancho actual de la caja
-      const height = width / aspectRatio; // Calcula la altura según la proporción
-      box.style.height = `${height}px`; // Aplica la altura calculada
-    });
-  }
+  boxes.forEach(box => {
+    const width = box.offsetWidth; 
+    const height = width / aspectRatio; 
+    box.style.height = `${height}px`; 
+  });
 
-  // Ajustar las cajas al cargar la página y al cambiar el tamaño de la ventana
-  window.onload = adjustBoxSizes;
-  window.onresize = adjustBoxSizes;
+  preboxes.forEach(prebox => {
+    const width = prebox.offsetWidth; 
+    const height = width / aspectRatio; 
+    prebox.style.height = `${height}px`; 
+  });
+}
+
+// Ajustar las cajas al cargar la página y al cambiar el tamaño de la ventana
+window.onload = adjustBoxSizes;
+window.onresize = adjustBoxSizes;
