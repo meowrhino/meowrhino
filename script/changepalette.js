d62ee30220ca8c26fa312dd65a83d0dcaacd952f
manu <manuellatourf@gmail.com>
2025-05-30 19:10:18 +0200

    probando cositas

--- script/changepalette.js
+++
@@ -1,9 +0,0 @@
-function applyPalette(paletteName) {
-    // Iterar sobre los colores de la paleta y aplicarlos a las variables principales
-    for (let i = 1; i <= 4; i++) {
-      const paletteVariable = `--${paletteName}-color-${i}`;
-      const mainVariable = `--color-${i}`;
-      const colorValue = getComputedStyle(document.documentElement).getPropertyValue(paletteVariable).trim();
-      document.documentElement.style.setProperty(mainVariable, colorValue);
-    }
-  }
